209eda426fdcaf9da5c4c7266e3a6dc4307ef091
student <student>
2017-01-21 14:19:41 -0500

    Add Visitor

--- js/MainGameState.js
+++ js/MainGameState.js
@@ -9,10 +9,16 @@ var board_walls = [
 	[1,1,1,1,1,0,0,0,0,1,0,1,1],
 	[1,1,1,1,1,0,0,0,0,1,0,0,1],
 	[1,0,0,0,0,0,1,1,1,1,0,0,1],
+	[1,0,1,1,1,1,1,1,1,1,0,0,1],
 	[1,0,0,0,0,0,0,0,0,0,0,0,1],
-	[1,0,0,0,0,0,1,1,1,1,1,1,1],
-	[1,1,1,1,1,1,1,1,1,1,1,1,1]
+	[1,0,0,0,0,0,1,1,1,1,1,1,1]
 ];
+
+var U = 0;
+var D = 1;
+var L = 2;
+var R = 3;
+var path = [R,R,U,U,U,U,R,R,D,R,D,D,D,R,D,D,L,L,D,L,L,L,L,D,D,D,R,R,R,R,U,R,R,R,R,R,R,U,L,U,U,U,U,U,U,U,L,U,U,R,D,R,R];
 
 var MainGameState = function(){};
 
@@ -34,6 +40,7 @@ MainGameState.prototype = {
 
 		this.load.image('add','assets/addActor.png');
 		this.load.image('selected','assets/selected.png');
+		this.load.image('visitor','assets/visitor.png');
 	},
 	create : function() {
 			game.stage.backgroundColor = 0xFFFFFF;
@@ -57,17 +64,28 @@ MainGameState.prototype = {
 					if(board_walls[i][j] === 0){
 						this.board[i].push({
 	    				orientation: "up",
-	            sprite: this.add.button(150 + 50*j,50*i,'floor')
+	            sprite: this.add.button(150 + 50*j,50*i,'floor', this.onClickBoard, this)
 	    			});
 					}else{
-							this.board[i].push({
-		    				orientation: "up",
-		            sprite: this.add.button(150 + 50*j,50*i,'wall')
-		    			});
+						this.board[i].push({
+							orientation: null,
+							sprite: null
+						});
 					}
         }
       }
 
+			//visitor
+
+			this.visitorStep = 0;
+			this.startVisitorPos = {
+				x: 150 + 50,
+			 	y: 50*5
+			};
+			this.visitor = this.add.sprite(this.startVisitorPos.x, this.startVisitorPos.y, "visitor");
+			this.visitor.scared = 0;
+
+			game.time.events.loop(250, this.moveVisitor, this);
       //state vars
       this.currentActor = "ghost";
       this.currentOrientation = "up";
@@ -76,6 +94,55 @@ MainGameState.prototype = {
 	},
 
 	update : function () {
+
+	},
+
+	moveVisitor : function(){
+		var nextStep = path[this.visitorStep];
+		if(nextStep === U)
+			this.visitor.position.y -= 50;
+		if(nextStep === D)
+			this.visitor.position.y += 50;
+		if(nextStep === L)
+			this.visitor.position.x -= 50;
+		if(nextStep === R)
+			this.visitor.position.x += 50;
+
+		this.evaluateVisitorScareFactor();
+
+		this.visitorStep++;
+		if(this.visitorStep >= path.length)
+			this.resetVisitor();
+	},
+
+	resetVisitor : function(){
+		this.visitorStep = 0;
+		this.visitor.position.x = this.startVisitorPos.x;
+		this.visitor.position.y = this.startVisitorPos.y;
+	},
+
+	evaluateVisitorScareFactor : function(){
+		var vx = this.visitor.position.x;
+		var vy = this.visitor.position.y;
+
+		var actorsDistFactor = 0;
+
+		for(var rowI in this.board){
+			for(var colI in this.board[rowI]){
+				var sprite = this.board[rowI][colI].sprite;
+				if(sprite !== null && sprite.key !== "floor"){
+					var sx = sprite.position.x;
+					var sy = sprite.position.y;
+					var dist = Math.sqrt((vx - sx)*(vx - sx) + (vy - sy)*(vy - sy));
+
+					// if its far away, its not scary
+					var distToThisActorFactor = Math.pow(2, -1*dist/50);
+					actorsDistFactor = 1 - actorsDistFactor*(1-distToThisActorFactor);
+				}
+			}
+		}
+		var scareFactor = 1 - distNonScaryFactor;
+		console.log("SCARYNESS: " + scareFactor);
 	},
 
   onClickActor : function (button) {
@@ -87,6 +154,23 @@ MainGameState.prototype = {
   },
 
   onClickAdd : function (button) {
-		this.board[3][4].sprite.loadTexture(this.currentActor);
+		// don't add if nothing selected
+		if(this.selected.visible === false)
+			return;
+
+		// update sprite at location of the selected box
+		this.board[
+			(this.selected.position.y)/50
+		][
+			(this.selected.position.x - 150)/50
+		].sprite.loadTexture(this.currentActor);
+
+		// hide the selected indicator after placing an item
+		this.selected.visible = false;
+  },
+
+  onClickBoard : function (button) {
+		this.selected.visible = true;
+		this.selected.position = button.position;
   }
 };
